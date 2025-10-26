66d28021d68138cb52cd3d1c10bf702c45d3f1e5
kizawa825 <kizawa@mihachi.com>
2025-10-26 23:54:29 +0900

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://siam-healing.example.com',
   output: 'static',
-  adapter: cloudflare(),
   scopedStyleStrategy: 'where'
 });
